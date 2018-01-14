301a2d423530ff9bb7e3516018d76374d221b8d8
Ekaterina Dontsova <ekaterina.dontsova@gmail.com>
2018-01-13 16:43:40 -0800

    meeting rsvp - park and user

--- modules/routes.js
+++ modules/routes.js
@@ -11,9 +11,9 @@ module.exports = function (express, app, path, bcrypt, dbClient) {
 	}*/
 
 	app.use(function(req, res, next) {
-          res.locals.user = req.session.user;
-          next();
-    });
+	  	res.locals.user = req.session.user;
+	  	next();
+	});
 
 	app.get("/", /*checkAuth,*/ function (req, res, next) {
     		res.render("index");
@@ -74,7 +74,7 @@ module.exports = function (express, app, path, bcrypt, dbClient) {
 											} else {
 												req.session.user = resultInsert.rows[0]["id"];
 												req.session.authenticated = true;
-												res.status(200).send({"message" : "user registered: " + resultInsert.rows[0].id});
+												res.status(200).send({"message" : "user registered: " + resultInsert.rows[0].id, "locals" : {"user" : resultInsert.rows[0].id}});
 											}
 										});
 									});
@@ -101,13 +101,12 @@ module.exports = function (express, app, path, bcrypt, dbClient) {
 				if (err){
 					res.status(500).send({"error" : "Error to find user in DB."});
 				} else if(result.rows.length == 1) {
-				console.log(444);
 					var user = result.rows[0];
 					bcrypt.compare(password, user.password, function(errBcrypt, resultBcrypt) {
 						if(resultBcrypt) {
 							req.session.user = user.id;
 							req.session.authenticated = true;
-							res.status(200).send({"message" : "user authorised: " + user.id});
+							res.status(200).send({"message" : "user authorised: " + user.id, "locals" : {"user" : user.id}});
 						} else {
 							res.status(500).send({"error" : "Password is incorrenct."});
 						}
@@ -133,53 +132,33 @@ module.exports = function (express, app, path, bcrypt, dbClient) {
 		}
     });
 
-    app.post("/rsvp", function (req, res, next) { // TODO: change for Parks table
+    app.post("/rsvp", function (req, res, next) {
+    	//var query = "drop table meetings";
+    	var query = "create table if not exists meetings(id serial primary key, parkId varchar(255) not null, userID integer not null, date date not null, time time not null)";
+		dbClient.query(query, (errCrTb, resultCrTb) => {
+			if (errCrTb){
+				console.log("Error create table: " + err);
+				res.status(500).send({"error" : "Error create table: " + errCrTb});
+			} else {
+				var park = req.body.park.trim();
+				var user = req.body.user.trim();
+				var date = req.body.date.trim();
+				var time = req.body.time.trim();
 
-    		dbClient.query("create table if not exists users(id serial primary key, login varchar(40) not null, password varchar(65) not null, name varchar(255), children varchar(255))", (err, result) => {
-
-    			if (err){
-    				console.log("Error create table: " + err);
-    				res.status(500).send({"error" : "Error create table: " + err});
-    			} else {
-    				var login = req.body.login.trim();
-    				var password1 = req.body.password1.trim();
-    				var password2 = req.body.password2.trim();
-    				var name = req.body.firstName.trim() + " " + req.body.lastName.trim();
-    				var children = req.body.children.trim();
-
-    				if(login !=""){
-    					dbClient.query("select id from users where login = '" + login + "'", (errSelect, resultSelect) => {
-    						if (errSelect){
-    							console.log("Error find user: " + errSelect);
-    							res.render("login", {"errorMessage" : "Error check if user exists."});
-    						} else {
-    							if(resultSelect.rows.length > 0){
-    								res.status(500).send({"error" : "This login is already exists."});
-    							} else {
-    								if(password1 == password2){ // ok
-    									bcrypt.hash(password1, 10, function(err, passHash) {
-                                        	dbClient.query("insert into users (login, password, name, children) values ('"+ login + "', '"+ passHash + "', '"+ name + "', '"+ children +"') returning *", (errInsert, resultInsert) => {
-    											if (errInsert){
-    												console.log("Error insert user: " + errInsert);
-    											} else {
-    												req.session.user = resultInsert.rows[0]["id"];
-    												req.session.authenticated = true;
-    												res.status(200).send({"message" : "user registered: " + resultInsert.rows[0].id});
-    											}
-    										});
-    									});
-    								} else {
-    									res.status(500).send({"error" : "Passwords are not the same."});
-    								}
-    							}
-    						}
-    					});
-    				} else {
-    					res.status(500).send({"error" : "Login should not be empty."});
-    				}
-    			}
-    		});
-    	});
+				if(date !="" && time !=""){
+					dbClient.query("insert into meetings (parkId, userId, date, time) values ('"+ park + "', '"+ user + "', '"+ date + "', '"+ time +"') returning *", (errInsert, resultInsert) => {
+						if (errInsert){
+							console.log("Error insert meeting: " + errInsert);
+						} else {
+							res.status(200).send({"message" : "meeting registered: " + resultInsert.rows[0].id});
+						}
+					});
+				} else {
+					res.status(500).send({"error" : "Login should not be empty."});
+				}
+			}
+		});
+    });
 
    	app.get("*", function(req, res){
    		res.status(404).send("Can not find the page");
